5089c1b12e35b5cd7af0e3570098e0657001908d
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-13 14:19:36 -0300

    Todo List and Form

--- frontend/src/todo/toDo.jsx
+++ frontend/src/todo/toDo.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoList from './toDoList'
+import TodoForm from './toDoForm'
 
 const Todo = (props) => {
     return(
         <div>
             <PageHeader name="Tarefas" small="Cadastro"/>
+            <TodoForm/>
+            <TodoList/>
         </div>
     )
 }
