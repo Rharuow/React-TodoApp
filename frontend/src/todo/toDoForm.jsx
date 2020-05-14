81c8e77097ff0941c6cae53ac52426b3fcec39c3
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-14 17:44:53 -0300

    shortcut

--- frontend/src/todo/toDoForm.jsx
+++ frontend/src/todo/toDoForm.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role="form" className="todoForm">
+export default props => { 
+    
+    const keyHandler = (e) => {
+        if (e.key === "Enter") {
+            e.shiftKey ? props.handleSearch() : props.action()
+        } else if(e.key === "Escape") {
+            props.handleClear()
+        }
+    }
+    
+    return(
+        <div role="form" className="todoForm">
 
         <Grid cols='12 9 10'>
-            <input className="form-control" onChange={props.descriptionAction} id="description" placeholder="Adicione uma tarefa" value={props.value}/>
+            <input className="form-control" onChange={props.descriptionAction} id="description" placeholder="Adicione uma tarefa" onKeyUp={keyHandler} value={props.value}/>
         </Grid>
 
         <Grid cols='12 3 2'>
@@ -15,5 +25,6 @@ export default props => (
             <IconButton style="default" icon="close" onClick={props.handleClear}/>
         </Grid>
 
-    </div>
-)
+    </div>   
+    )
+}
